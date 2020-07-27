680d306823abeb51578b30879f93313fd4782ee2
sologuboved <sologuboved@gmail.com>
2020-07-27 17:42:53 +0300

    working on a href

--- lj_post_scraper.py
+++ lj_post_scraper.py
@@ -9,11 +9,16 @@ from helpers import counter
 class PostScraper:
     def __init__(self, post_url):
         self.post_url = post_url
+        self.post = dict()
         self.comments = list()
 
     def scrape_post(self):
         soup = BeautifulSoup(requests.get(self.post_url).text, 'lxml')
-        print(soup)
+        self.post['title'] = soup.find_all('title')[0].text
+        post = process_links(soup.find_all('article', {'class': "b-singlepost-body entry-content e-content"})[0])
+        self.post['post'] = BeautifulSoup(post, 'lxml').text
+        self.post['date'] = soup.find_all('time', {'class': "b-singlepost-author-date published dt-published"})[0].text
+        pprint(self.post)
 
     def scrape_threads(self):
         contents = get_contents(self.post_url)
@@ -47,7 +52,15 @@ def get_contents(url):
     )[0].strip()[:-1])
 
 
+def process_links(soup):
+    soup = str(soup)
+    res = re.sub(r"<a href=(.+?)>(.+?)</a>", r"[a href=\1]\2[/a]", soup, flags=re.DOTALL)
+    print(res)
+    return soup
+
+
 if __name__ == '__main__':
+    # scraper = PostScraper('https://formerchild.livejournal.com/39186.html')
     scraper = PostScraper('https://formerchild.livejournal.com/39619.html')
     # scraper = PostScraper('https://baaltii1.livejournal.com/198675.html')
     # scraper.scrape_thread('https://formerchild.livejournal.com/39619.html?thread=127939#t127939')
