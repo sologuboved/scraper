3b6341aeff703ac37cd7754d8d7e747f01181c49
sologuboved <sologuboved@gmail.com>
2021-09-09 17:38:45 +0300

    can now scrape from entry

--- blog2channel.py
+++ blog2channel.py
@@ -1,28 +1,33 @@
+from functools import partial
+from pprint import pprint
 import time
 import requests
 from bs4 import BeautifulSoup
 from telegram import Bot, ParseMode
-from secrets import TOKEN, MY_CHANNEL_ID, MY_BLOG_URL
+from userinfo import TOKEN, MY_CHANNEL_ID, MY_BLOG_URL, MY_BLOG_PAGE_URL
 
 
-def post_to_channel(page_num):
-    for url, title in scrape_blog(page_num):
+def post_to_channel(scraper):
+    for url, title in scraper():
         text = '<a href="{}">{}</a>'.format(url, title)
         Bot(token=TOKEN).send_message(chat_id=MY_CHANNEL_ID, text=text, parse_mode=ParseMode.HTML)
         time.sleep(10)
 
 
-def scrape_blog(page_num):
+def scrape_blog_from_page(page_num):
     links = list()
     while page_num >= 1:
+        print(page_num)
         links.extend(scrape_page(page_num))
         page_num -= 1
+    print(f"Scraped {len(links)} links")
+    pprint(links)
     return links
 
 
 def scrape_page(page_num):
     links = list()
-    items = BeautifulSoup(requests.get(MY_BLOG_URL.format(page_num)).content, 'lxml').find_all(
+    items = BeautifulSoup(requests.get(MY_BLOG_PAGE_URL.format(page_num)).content, 'lxml').find_all(
         'h2', {'class': 'entry-title'}
     )
     for item in items:
@@ -31,5 +36,28 @@ def scrape_page(page_num):
     return reversed(links)
 
 
+def scrape_blog_from_entry(entry_url):
+    entry_url = MY_BLOG_URL.format(entry_url)
+    links = [(
+        entry_url,
+        BeautifulSoup(requests.get(entry_url).content, 'lxml').find('h1', {'class': 'entry-title'}).text.strip()
+    )]
+    next_url = entry_url
+    while True:
+        print(next_url)
+        try:
+            next_entry = BeautifulSoup(requests.get(next_url).content, 'lxml').find(
+                'div', {'class': 'nav-next'}
+            ).find('a', {'href': True})
+        except AttributeError:
+            break
+        next_url = next_entry.get('href')
+        links.append((next_url, next_entry.text.split("Next post")[-1].strip()))
+    print(f"Scraped {len(links)} links")
+    pprint(links)
+    return links
+
+
 if __name__ == '__main__':
-    post_to_channel(7)
+    # post_to_channel(partial(scrape_blog_from_page, 7))
+    post_to_channel(partial(scrape_blog_from_entry, 'https://generalfailure.site/2021/02/13/virgil-not-virgin/'))
